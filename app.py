1447aa6c720ebceb850fd572d070e8f94b8cd04f
samistart <samistart@hotmail.com>
2016-11-21 01:17:39 +0000

    created sidebar and for blog and inherited view for an article

--- app.py
+++ app.py
@@ -1,20 +1,39 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, Markup, redirect, url_for
 import datetime
 from models import Entry
 from database import db
+from models import DBSession
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db.init_app(app)
 
-
 @app.route('/')
+def homepage():
+    return redirect(url_for("blog"))
+
+
 @app.route('/blog/')
 def blog():
-    entry = Entry(title='title', slug='slug', content='content', published=True, timestamp=datetime.datetime.now())
-    print(entry.title)
-    entries = ["london", "paris", "berlin"]
-    return render_template("blog.html", entries=entries)
+    session = DBSession()
+    first_entry = session.query(Entry).first()
+    return redirect(url_for('.article', slug=first_entry.slug))
+
+
+@app.route('/blog/<path:slug>')
+def article(slug):
+    session = DBSession()
+    raw_entries = session.query(Entry)
+    entries = []
+    for raw_entry in raw_entries:
+        entry = {
+            'title' : raw_entry.title,
+            'slug' : raw_entry.slug
+        }
+        entries.append(entry)
+    current_article = session.query(Entry).filter_by(slug=slug).first()
+    current_article.timestamp = current_article.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+    return render_template("articles/" + current_article.slug + ".html", entries=entries, current_article=current_article)
 
 
 @app.route('/about/')
--- models.py
+++ models.py
@@ -12,11 +12,9 @@ Base = declarative_base()
 
 class Entry(Base):
     __tablename__ = 'Entry'
-    title = Column('Title', String())
-    slug = Column('Slug', String(), primary_key=True)
-    content = Column('Content', String())
-    published = Column('Published', Boolean())
-    timestamp = Column('Timestamp', DateTime, default=datetime.datetime.now, index=True)
+    title = Column('Title', String(), nullable=False)
+    slug = Column('Slug', String(), primary_key=True, nullable=False)
+    timestamp = Column('Timestamp', DateTime, default=datetime.datetime.now, index=True, nullable=False)
 
     def __init__(self, title, slug, content, published, timestamp):
         self.title = title
@@ -24,15 +22,6 @@ class Entry(Base):
         self.content = content
         self.published = published
         self.timestamp = timestamp
-
-    # def save(self, *args, **kwargs):
-    #     if not self.slug:
-    #         self.slug = re.sub('[^\w]+', '-', self.title.lower())
-    #     ret = super(Entry, self).save(*args, **kwargs)
-    #
-    #     # Store search content.
-    #     self.update_search_index()
-    #     return ret
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
@@ -43,19 +32,3 @@ Base.metadata.create_all(engine)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
-# A DBSession() instance establishes all conversations with the database
-# and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
-session = DBSession()
-
-# Insert a Person in the person table
-# new_entry = Entry(title='title', slug='slug', content='content', published=True, timestamp=datetime.datetime.now())
-# session.add(new_entry)
-# session.commit()
-
-print(session.query(Entry).first().timestamp)
-
